ec0321f33d519596d50425f8906afee18f625490
Saeid Akbari <saeidscorp@yahoo.com>
2016-09-12 10:29:22 +0430

    bare-bones init.

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject cljbuild "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.immutant/web "2.1.5"]
+                 [ring "1.5.0"]
+                 [environ "1.1.0"]]
+  :main ^:skip-aot cljbuild.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ src/cljbuild/core.clj
@@ -0,0 +1,13 @@
+(ns cljbuild.core
+  (:require [immutant.web :refer [run]]
+            [environ.core :refer [env]]
+            [ring.util.response :as response])
+  (:gen-class))
+
+(defn make-handler []
+  (-> (fn [req] (response/response "Hello World!"))))
+
+(defn -main [& args]
+  (println "Starting server...")
+  (run (make-handler) {:port (env :openshift-diy-port 8082) :host (env :openshift-diy-ip "127.0.0.1")})
+  (println "Server started."))
